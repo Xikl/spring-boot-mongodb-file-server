fc0c70462fdcbb7590e5da2388baea9ae65eb1cc
X1mo <1392936858@qq.com>
2018-05-15 22:32:01 +0800

    设计spring boot版本

--- src/main/java/com/ximo/fileserver/config/SecurityConfig.java
+++ src/main/java/com/ximo/fileserver/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.ximo.fileserver.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author 朱文赵
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 @EnableWebMvc
-public class SecurityConfig extends WebMvcConfigurerAdapter{
+public class SecurityConfig implements WebMvcConfigurer {
 
     /**
      * 允许跨域请求
